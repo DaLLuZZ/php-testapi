d7cb1f7bc838aab4b79f6abcf65e7e1ca0f0e7d2
Bara <mail@bara.dev>
2020-12-24 23:49:46 +0100

    Add simple API_KEY check
    Example: https://api.url.tld?API_KEY=XXXX

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -7,15 +7,25 @@ use Illuminate\Support\Facades\DB;
 
 class PlayerController extends Controller
 {
-    public function Index()
+    public function Index(Request $request)
     {
+        if ($request->API_KEY != env('API_KEY'))
+        {
+            return response()->json('Unauthorized', 401);
+        }
+
         $players = DB::table('Player')->select('Id', 'Name', 'IsActive')->get();
 
         return response()->json($players);
     }
 
-    public function GetPlayer($playerid)
+    public function GetPlayer(Request $request, $playerid)
     {
+        if ($request->API_KEY != env('API_KEY'))
+        {
+            return response()->json('Unauthorized', 401);
+        }
+
         $player = DB::table('Player')->select('Id', 'Name', 'IsActive')->where('Id', $playerid)->first();
 
         return response()->json($player);
