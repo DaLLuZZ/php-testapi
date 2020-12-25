39258caf6771aea08f8ff5a739a9a8fd7e5281ee
Bara <mail@bara.dev>
2020-12-25 17:57:18 +0100

    Return request with Id of the new map

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -48,9 +48,9 @@ class MapController extends Controller
             return response()->json('Unauthorized', 401);
         }
 
-        DB::table('Map')->insert(['Id' => $request->Id, 'Name' => $request->Name, 'Tier' => $request->Tier, 'IsActive' => $request->IsActive]);
+        $request['Id'] = DB::table('Map')->insertGetId(['Name' => $request->Name, 'Tier' => $request->Tier, 'IsActive' => $request->IsActive]);
 
-        return response()->json('Created', 201);
+        return response()->json($request, 201);
     }
 
     public function UpdateMap(Request $request, $MapId)
