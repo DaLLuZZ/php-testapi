b9bf1d0cbd0806c2012d442de6771a9679bd9e51
B3none <ablackham2000@gmail.com>
2021-03-06 15:23:22 +0000

    Switch out old array syntax in create player timing table migration

--- database/migrations/2020_12_23_233134_create_player_timing_table.php
+++ database/migrations/2020_12_23_233134_create_player_timing_table.php
@@ -25,7 +25,7 @@ class CreatePlayerTimingTable extends Migration
             $table->boolean('IsRanked')->default(1);
             $table->dateTimeTz('CreatedDate')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->dateTimeTz('LastModifiedDate')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
-            $table->unique(array('PlayerId', 'MapId', 'StyleId', 'ZoneType', 'ZoneOrdinal'));
+            $table->unique(['PlayerId', 'MapId', 'StyleId', 'ZoneType', 'ZoneOrdinal']);
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_unicode_ci';
