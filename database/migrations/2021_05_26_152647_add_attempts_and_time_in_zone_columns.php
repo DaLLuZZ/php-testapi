55f319464eaa418e50a06678d7527b73527aa5ac
Bara <mail@bara.dev>
2021-05-26 17:30:06 +0200

    Add Attempts and TimeInZone column

---
+++ database/migrations/2021_05_26_152647_add_attempts_and_time_in_zone_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAttemptsAndTimeInZoneColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('PlayerTiming', function (Blueprint $table) {
+            $table->unsignedInteger('TimeInZone')->after('Duration');
+            $table->unsignedInteger('Attempts')->after('TimeInZone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('PlayerTiming', function (Blueprint $table) {
+            $table->dropColumn('TimeInZone');
+            $table->dropColumn('Attempts');
+        });
+    }
+}
