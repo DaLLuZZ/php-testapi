2ab70a3a006c1c97165052156e1041963bdcdd62
Bara <mail@bara.dev>
2021-08-06 15:33:29 +0200

    Update PlayerSeeder.php

--- database/seeders/PlayerSeeder.php
+++ database/seeders/PlayerSeeder.php
@@ -18,8 +18,10 @@ class PlayerSeeder extends Seeder
     {
         for ($i=0; $i < 100000; $i++) {
             DB::table('Player')->insert([
-                'Id' => random_int(1000000, 999999999),
+                'Id' => random_int(100000, 999999999),
+                'CommunityId' => random_int(100000, 999999999),
                 'Name' => Str::random(32),
+                'Status' => random_int(0, 1)
             ]);
         }
     }
