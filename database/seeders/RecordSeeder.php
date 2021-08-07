a7958dc9f2a8f7b973bb96c49b9df6d83a687182
Bara <mail@bara.dev>
2021-08-07 12:30:15 +0200

    Create 100 stage or checkpoints times per main time

--- database/seeders/RecordSeeder.php
+++ database/seeders/RecordSeeder.php
@@ -15,15 +15,19 @@ class RecordSeeder extends Seeder
     public function run()
     {
         // Main times
-        for ($i=0; $i < 100000; $i++) {
+        for ($main=0; $main < 100000; $main++) {
+            $map = random_int(1, 500);
+            $style = random_int(1, 10);
+            $level = random_int(0, 10);
+
             $id = DB::table('PlayerTiming')->insertGetId([
-                'MapId' => random_int(1, 500),
-                'PlayerId' => $i,
-                'StyleId' => random_int(1, 10),
-                'Level' => random_int(0, 10),
+                'MapId' => $map,
+                'PlayerId' => $main,
+                'StyleId' => $style,
+                'Level' => $level,
                 'Tickrate' => 100,
-                'Time' => random_int(10, 200) / random_int(30, 60),
-                'TimeInZone' => random_int(1, 10) / random_int(3, 9),
+                'Time' => random_int(10, 200) / M_PI,
+                'TimeInZone' => random_int(1, 10) / M_PI,
                 'Attempts' => random_int(1, 20),
                 'Status' => 0
             ]);
@@ -49,69 +53,72 @@ class RecordSeeder extends Seeder
                 'EndVelocityY' => random_int(0, 255),
                 'EndVelocityZ' => random_int(0, 255),
             ]);
-        }
 
-        // Stage times
-        for ($i=0; $i < 100000; $i++) {
-            $id = DB::table('PlayerTimingStages')->insertGetId([
-                'MapId' => random_int(1, 500),
-                'PlayerId' => $i,
-                'StyleId' => random_int(1, 10),
-                'Level' => random_int(0, 10),
-                'Tickrate' => 100,
-                'Time' => random_int(10, 200) / random_int(30, 60),
-                'TimeInZone' => random_int(1, 10) / random_int(3, 9),
-                'Attempts' => random_int(1, 20),
-                'Status' => 0
-            ]);
+            if ($main % 2 == 0) {
+                // Stage times
+                for ($stage=0; $stage < 100; $stage++) {
+                    $id = DB::table('PlayerTimingStages')->insertGetId([
+                        'MapId' => $map,
+                        'PlayerId' => $main,
+                        'StyleId' => $style,
+                        'Level' => $level+$stage,
+                        'Tickrate' => 100,
+                        'Time' => random_int(10, 200) / M_PI,
+                        'TimeInZone' => random_int(5, 10) / M_PI,
+                        'Attempts' => random_int(1, 20),
+                        'Status' => 0
+                    ]);
 
-            DB::table('PlayerTimingStageInsight')->insert([
-                'PlayerTimingStageId' => $id,
-                'StartPositionX' => random_int(0, 255),
-                'StartPositionY' => random_int(0, 255),
-                'StartPositionZ' => random_int(0, 255),
-                'EndPositionX' => random_int(0, 255),
-                'EndPositionY' => random_int(0, 255),
-                'EndPositionZ' => random_int(0, 255),
-                'StartAngleX' => random_int(0, 255),
-                'StartAngleY' => random_int(0, 255),
-                'StartAngleZ' => random_int(0, 255),
-                'EndAngleX' => random_int(0, 255),
-                'EndAngleY' => random_int(0, 255),
-                'EndAngleZ' => random_int(0, 255),
-                'StartVelocityX' => random_int(0, 255),
-                'StartVelocityY' => random_int(0, 255),
-                'StartVelocityZ' => random_int(0, 255),
-                'EndVelocityX' => random_int(0, 255),
-                'EndVelocityY' => random_int(0, 255),
-                'EndVelocityZ' => random_int(0, 255),
-            ]);
-        }
-        
-        // Checkpoint times
-        for ($i=0; $i < 100000; $i++) {
-            $id = DB::table('PlayerTimingCheckpoints')->insertGetId([
-                'MapId' => random_int(1, 500),
-                'PlayerId' => $i,
-                'StyleId' => random_int(1, 10),
-                'Level' => random_int(0, 10),
-                'Tickrate' => 100,
-                'Time' => random_int(10, 200) / random_int(30, 60),
-                'Status' => 0
-            ]);
+                    DB::table('PlayerTimingStageInsight')->insert([
+                        'PlayerTimingStageId' => $id,
+                        'StartPositionX' => random_int(0, 255),
+                        'StartPositionY' => random_int(0, 255),
+                        'StartPositionZ' => random_int(0, 255),
+                        'EndPositionX' => random_int(0, 255),
+                        'EndPositionY' => random_int(0, 255),
+                        'EndPositionZ' => random_int(0, 255),
+                        'StartAngleX' => random_int(0, 255),
+                        'StartAngleY' => random_int(0, 255),
+                        'StartAngleZ' => random_int(0, 255),
+                        'EndAngleX' => random_int(0, 255),
+                        'EndAngleY' => random_int(0, 255),
+                        'EndAngleZ' => random_int(0, 255),
+                        'StartVelocityX' => random_int(0, 255),
+                        'StartVelocityY' => random_int(0, 255),
+                        'StartVelocityZ' => random_int(0, 255),
+                        'EndVelocityX' => random_int(0, 255),
+                        'EndVelocityY' => random_int(0, 255),
+                        'EndVelocityZ' => random_int(0, 255),
+                    ]);
+                }
+            }
+            else {
+                // Checkpoint times
+                for ($checkpoint=0; $checkpoint < 100; $checkpoint++) {
+                    $id = DB::table('PlayerTimingCheckpoints')->insertGetId([
+                        'MapId' => $map,
+                        'PlayerId' => $main,
+                        'StyleId' => $style,
+                        'Level' => $level+$checkpoint,
+                        'Tickrate' => 100,
+                        'Time' => random_int(10, 200) / M_PI,
+                        'Status' => 0
+                    ]);
 
-            DB::table('PlayerTimingCheckpointInsight')->insert([
-                'PlayerTimingCheckpointId' => $id,
-                'PositionX' => random_int(0, 255),
-                'PositionY' => random_int(0, 255),
-                'PositionZ' => random_int(0, 255),
-                'AngleX' => random_int(0, 255),
-                'AngleY' => random_int(0, 255),
-                'AngleZ' => random_int(0, 255),
-                'VelocityX' => random_int(0, 255),
-                'VelocityY' => random_int(0, 255),
-                'VelocityZ' => random_int(0, 255),
-            ]);
+                    DB::table('PlayerTimingCheckpointInsight')->insert([
+                        'PlayerTimingCheckpointId' => $id,
+                        'PositionX' => random_int(0, 255),
+                        'PositionY' => random_int(0, 255),
+                        'PositionZ' => random_int(0, 255),
+                        'AngleX' => random_int(0, 255),
+                        'AngleY' => random_int(0, 255),
+                        'AngleZ' => random_int(0, 255),
+                        'VelocityX' => random_int(0, 255),
+                        'VelocityY' => random_int(0, 255),
+                        'VelocityZ' => random_int(0, 255),
+                    ]);
+                }
+            }
         }
     }
 }
