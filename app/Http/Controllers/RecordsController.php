f0d1e0e9487b3f0e6924f100210af6bec7f6d970
Bara <mail@bara.dev>
2021-08-07 18:22:21 +0200

    Merge Insight data into player time array

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -15,7 +15,7 @@ class RecordsController extends Controller
             get the player id
             select the other runs like checkpoints/stages
         */
-        $records = DB::table('PlayerTiming')
+        $records = DB::table('PlayerTimingStages')
                         ->select('*')
                         ->where('MapId', $MapId)
                         ->groupBy(['StyleId', 'Level'])
@@ -29,7 +29,7 @@ class RecordsController extends Controller
 
     public function GetMapPlayerRecord(Request $request, $MapId, $PlayerId)
     {
-        $records = DB::table('PlayerTiming')
+        $records = DB::table('PlayerTimingStages')
                         ->select('*')
                         ->where('MapId', $MapId)
                         ->where('PlayerId', $PlayerId)
@@ -39,6 +39,22 @@ class RecordsController extends Controller
 
         $this->checkExists($records);
 
-        return response()->json($records);
+    $newRecords = [];
+    
+    foreach ($records as $record)
+    {
+        $insight = DB::table('PlayerTimingStageInsight')
+                        ->select('*')
+                        ->where('PlayerTimingStageId', $record->Id)
+                        ->first();
+
+        $record = (array)$record;
+        $insight = (array)$insight;
+
+        array_push($record, $insight);
+        array_push($newRecords, $record);
+    }
+
+        return response()->json($newRecords);
     }
 }
