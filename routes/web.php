3cec659fd83bb059774d9448a69a4cfa2df021de
Bara <mail@bara.dev>
2021-11-04 23:10:46 +0100

    Remove unused routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -55,43 +55,6 @@ $router->group(['prefix' => 'Style', 'middleware' => 'auth'], function () use ($
     $router->delete('/Id/{StyleId}', 'StyleController@DeleteStyle');
 });
 
-$router->group(['prefix' => 'PlayerTiming', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('/', 'PlayerTimingController@Index');
-
-    $router->get('/TimingId/{TimingId}', 'PlayerTimingController@GetByTimingId');
-    $router->get('/MapId/{MapId}', 'PlayerTimingController@GetByMapId');
-    $router->get('/PlayerId/{PlayerId}', 'PlayerTimingController@GetByPlayerId');
-    $router->get('/StyleId/{StyleId}', 'PlayerTimingController@GetByStyleId');
-
-    $router->get('/MapId/{MapId}/PlayerId/{PlayerId}', 'PlayerTimingController@GetByMapPlayerId');
-    $router->get('/MapId/{MapId}/StyleId/{StyleId}', 'PlayerTimingController@GetByMapStyleId');
-    $router->get('/PlayerId/{PlayerId}/StyleId/{StyleId}', 'PlayerTimingController@GetByPlayerStyleId');
-
-    $router->get('/MapId/{MapId}/PlayerId/{PlayerId}/StyleId/{StyleId}', 'PlayerTimingController@GetByAll');
-
-    $router->post('/', 'PlayerTimingController@InsertPlayerTiming');
-    
-    $router->delete('/TimingId/{TimingId}', 'PlayerTimingController@DeleteByTimingId');
-    $router->delete('/MapId/{MapId}', 'PlayerTimingController@DeleteByMapId');
-    $router->delete('/PlayerId/{PlayerId}', 'PlayerTimingController@DeleteByPlayerId');
-    $router->delete('/StyleId/{StyleId}', 'PlayerTimingController@DeleteByStyleId');
-
-    $router->delete('/MapId/{MapId}/PlayerId/{PlayerId}', 'PlayerTimingController@DeleteByMapPlayerId');
-    $router->delete('/MapId/{MapId}/StyleId/{StyleId}', 'PlayerTimingController@DeleteByMapStyleId');
-    $router->delete('/PlayerId/{PlayerId}/StyleId/{StyleId}', 'PlayerTimingController@DeleteByPlayerStyleId');
-
-    $router->delete('/MapId/{MapId}/PlayerId/{PlayerId}/StyleId/{StyleId}', 'PlayerTimingController@DeleteByAll');
-});
-
-$router->group(['prefix' => 'PlayerTimingInsight', 'middleware' => 'auth'], function () use ($router) {
-    $router->get('/TimingId/{TimingId}', 'PlayerTimingInsightController@GetInsight');
-
-    $router->post('/', 'PlayerTimingInsightController@InsertInsight');
-
-    $router->delete('/InsightId/{InsightId}', 'PlayerTimingInsightController@DeleteInsightByInsightId');
-    $router->delete('/TimingId/{TimingId}', 'PlayerTimingInsightController@DeleteInsightByTimingId');
-});
-
 $router->group(['prefix' => 'PlayerSettings', 'middleware' => 'auth'], function () use ($router) {
     $router->get('/PlayerId/{PlayerId}/Setting/{SettingName}', 'PlayerSettingsController@GetValue');
 
